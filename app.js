9d7a228a4cea021f16a8b1d0550c0b7c84da265e
Rafael Moraes <rafael.msantos@sptech.school>
2024-10-28 21:22:52 -0300

    fim da primeira aula, conectando html ao js

---
+++ app.js
@@ -0,0 +1,5 @@
+function sortear() {
+  let quantidade = document.getElementById("quantidade").value;
+  let de = document.getElementById("de").value;
+  let ate = document.getElementById("ate").value;
+}

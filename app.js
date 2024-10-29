516936d4da50853167580ea8058ef88f8c8f42b2
Rafael Moraes <rafael.msantos@sptech.school>
2024-10-28 22:31:55 -0300

    terceira aula, reiniciando o jogo

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function sortear() {
 
   for (let i = 0; i < quantidade; i++) {
     numero = obterNumeroAleatorio(de, ate);
-    
+
     while (sorteados.includes(numero)) {
       numero = obterNumeroAleatorio(de, ate);
     }
@@ -17,8 +17,30 @@ function sortear() {
 
   let resultado = document.getElementById("resultado");
   resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+  alterarStatusBotao();
 }
 
 function obterNumeroAleatorio(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao() {
+  let botao = document.getElementById("btn-reiniciar");
+  if (botao.classList.contains("container__botao-desabilitado")) {
+    botao.classList.remove("container__botao-desabilitado");
+    botao.classList.add("container__botao");
+  } else {
+    botao.classList.remove("container__botao");
+    botao.classList.add("container__botao-desabilitado");
+  }
+}
+
+function reiniciar() {
+  quantidade = document.getElementById("quantidade").value = "";
+  de = document.getElementById("de").value = "";
+  ate = document.getElementById("ate").value = "";
+  resultado = document.getElementById(
+    "resultado"
+  ).innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>`;
+  alterarStatusBotao();
 }
